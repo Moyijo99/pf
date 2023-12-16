44f3e540aa29978fe155943188de198d9e6d7fc2
Aheedjo <maishanu.ahijo@yahoo.com>
2023-12-16 21:59:14 +0100

    Added the proffesional statatement section

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,13 @@ import WebTechs from "./components/WebTechs"
 import Projects from "./components/Projects"
 import About from "./components/About"
 import CTA from "./components/CTA"
+import ProffesionalStatement from "./components/ProffessionalStatement"
 
 export default function Home() {
   return (
     <>
         <Hero />
+        <ProffesionalStatement />
         <Skills />
         <WebTechs />
         <Projects id="projects" />
